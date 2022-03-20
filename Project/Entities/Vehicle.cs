57a3f53d29007db286e19710c0a1d5829e47d001
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-19 22:38:17 -0300

    feito sem interface

---
+++ Project/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+namespace Project.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Project.Entities;
+using Project.Services;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter rental data: ");
+            System.Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            System.Console.Write("Pickup (dd/MM/yyyy hh:MM): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            System.Console.Write("Return (dd/MM/yyyy hh:MM): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            System.Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+            System.Console.WriteLine();
+            System.Console.WriteLine("INVOICE:");
+            System.Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
